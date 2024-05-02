572baa548b2829e40dcc2c567df94d1ebdd45b1f
ryandri1314 <duongngv2203@gmail.com>
2024-05-02 23:47:02 +0700

    fix change step

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+
+const keyStep = 'key-step'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private dataService: DataService
   ) { }
+
+  ngOnInit(): void {
+    sessionStorage.setItem(keyStep, String(1))
+  }
   
   setLessonID(lessonId: number) {
     this.router.navigate(['/student/' + String(lessonId)]);
--- src/app/ui/student/dashboard/dashboard.component.ts
+++ src/app/ui/student/dashboard/dashboard.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+
+const keyStep = 'key-step'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   constructor(
     private router: Router,
     private dataService: DataService
   ) { }
+
+  ngOnInit(): void {
+    sessionStorage.setItem(keyStep, String(1))
+  }
+
   setLessonID(lessonId: number) {
     this.router.navigate(['/student/' + String(lessonId)]);
   }
